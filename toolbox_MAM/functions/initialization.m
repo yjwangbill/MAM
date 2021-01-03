34f1cae7abd20aa83f2678f05e412e729f819598
yjwangbill <52820264+yjwangbill@users.noreply.github.com>
2021-01-03 11:06:44 +0800

    Delete initialization.m

--- toolbox_MAM/functions/initialization.m
+++
@@ -1,8 +0,0 @@
-function [hyperGrad_aux,hyperGrad_aux_nu, theta] = initialization (param)
-
-theta =  proj_unitSimplex( ((1/param.inner.nGroups)*ones(param.inner.nFeatures,param.inner.nGroups) + .01*randn(param.inner.nFeatures,param.inner.nGroups))')'; 
-hyperGrad_aux.all   = zeros(param.inner.nFeatures,param.inner.nGroups,param.inner.nTasks);
-hyperGrad_aux.mean  = zeros(param.inner.nFeatures,param.inner.nGroups);             
-hyperGrad_aux_nu.all   = zeros(param.inner.nFeatures,1,param.inner.nTasks);
-hyperGrad_aux_nu.mean  = zeros(param.inner.nFeatures,1);
-end
